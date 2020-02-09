b9a6d03a0126ba95ebeb79d95accee8c63595263
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 15:22:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,8 +76,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  id = Movie.find_by({title: "Wat?"}).id
+  Movie.update(id, :title => 'Wat, huh?')
 end
 
 def can_update_multiple_items_at_once
