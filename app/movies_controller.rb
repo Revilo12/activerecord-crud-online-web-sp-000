95f378baff27a5db2e1c5bc4c0f0bf833535e630
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 14:50:48 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,6 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     binding.pry
   end
-  self
 end
 
 def can_get_the_first_item_in_the_database
